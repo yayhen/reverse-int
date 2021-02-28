c0953bbeaffaf251d71545ffd20f210b30f53af9
yayhen <mlpoknwow@gmail.com>
2021-02-28 11:18:27 +0300

    feat: implement task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let kString = n.toString();
+    let mArray = [];
+    mArray = kString.split('');
+    let mArray2 = mArray.reverse();
+    kString = mArray2.join('');
+    return parseInt(kString);
 }
